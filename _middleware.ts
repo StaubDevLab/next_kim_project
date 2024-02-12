d0e73ae1ffe398a5301488009225397e370f61b0
Kodomo At <guillaume@staub.pro>
2024-02-12 22:07:11 +0100

    Disabled middleware bug in Vercel

---
+++ _middleware.ts
@@ -0,0 +1,13 @@
+// import withAuth from "next-auth/middleware"
+// import { authOptions } from "@/lib/auth-options";
+//
+// export default withAuth({
+//     jwt: { decode: authOptions.jwt?.decode },
+//     callbacks: {
+//         authorized: ({ token }) => !!token,
+//     },
+// })
+//
+
+
+export const config = { matcher: ["/admin"] }
