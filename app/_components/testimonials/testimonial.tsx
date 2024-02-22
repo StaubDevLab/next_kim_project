c96d701d0af4ee2383be932607256765710a55b1
Kodomo At <guillaume@staub.pro>
2024-02-22 21:41:32 +0100

    Add dynamic content in testimonial component

--- app/_components/testimonials/testimonial.tsx
+++ app/_components/testimonials/testimonial.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {Testimonial} from "@prisma/client";
 import {Avatar, AvatarFallback} from "@/app/_components/ui/avatar";
+import {format} from "@formkit/tempo";
+
 type Props = {
-   testimonial:Testimonial
+    testimonial: Testimonial
 }
-export default function TestimonialComponent  ({testimonial} : Props)  {
+export default function TestimonialComponent({testimonial}: Props) {
+    function getInitials(str: string): string {
+
+        const lowerCaseStr = str.toUpperCase();
+
+
+        const words = lowerCaseStr.split(' ');
+
+
+        const initials = words.map((word) => word[0]);
+
+
+        return initials.join('');
+    }
+
     return (
         <div className="p-6 rounded-lg shadow-lg border">
-            <p>An audire commodo habemus cum. Ne sed corrumpit repudiandae. Tota
-                aliquip
-                democritum pro in, nec democritum intellegam ne. Propriae volutpat
-                dissentiet ea
-                sit, nec at lorem inani tritani, an ius populo perfecto
-                vituperatoribus. Eu cum
-                case modus salutandi, ut eum vocent sensibus reprehendunt.</p>
+            <p>{testimonial.opinion}</p>
             <div className="flex items-center mt-4 space-x-4">
                 <Avatar className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500">
-                    <AvatarFallback>CN</AvatarFallback>
+                    <AvatarFallback>{getInitials(testimonial.name)}</AvatarFallback>
                 </Avatar>
 
                 <div>
-                    <p className="text-lg font-semibold">Leroy Jenkins</p>
-                    <p className="text-sm dark:text-gray-400">Atelier méditation du 12/01/2024</p>
+                    <p className="text-lg font-semibold">{testimonial.name}</p>
+                    <p className="text-sm dark:text-gray-400">{testimonial.subject} du {format(testimonial.date as Date, "short", 'fr-FR')} </p>
                 </div>
             </div>
         </div>
