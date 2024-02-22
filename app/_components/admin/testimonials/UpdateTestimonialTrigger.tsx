1c05e26e8d8dc9ad9c1b04475895b186fc0932ed
Kodomo At <guillaume@staub.pro>
2024-02-22 21:34:38 +0100

    Bug to add category fixed, add admin panel for testimonials, forms to add and update

--- app/_components/admin/testimonials/UpdateTestimonialTrigger.tsx
+++ app/_components/admin/testimonials/UpdateTestimonialTrigger.tsx
@@ -3,16 +3,16 @@ import {useDispatch} from "react-redux";
 import {Button} from "@/app/_components/ui/button";
 import {open } from "@/stores/dialog-slice";
 
-import {Category} from "@prisma/client";
+import { Testimonial} from "@prisma/client";
 import React from "react";
 
-export function UpdateCategoryTrigger({children,category, handleOpen, handleCategoryData} : {children : React.ReactNode,category: Category|undefined, handleOpen: any, handleCategoryData: any}) {
+export function UpdateTestimonialTrigger({children,testimonial, handleOpen, handleTestimonialData} : {children : React.ReactNode,testimonial: Testimonial|undefined, handleOpen: any, handleTestimonialData: any}) {
 
 
     return (
         <div className="cursor-pointer" onClick={() => {
             handleOpen(true)
-            handleCategoryData(category)
+            handleTestimonialData(testimonial)
         }}>
             {children}
         </div>
