172ef134e8386b6c093b8ceb564048a6d6b4bcb4
Kodomo At <guillaume@staub.pro>
2024-03-18 15:27:54 +0100

    Add custom client email

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,23 +1,24 @@
 import {EmailClientTemplate} from '@/app/_components/email/email-client-template';
 import { Resend } from 'resend';
 import {EmailInfoTemplate} from "@/app/_components/email/email-info-template";
+import EmailToClientTemplateCustom from "@/app/_components/email/email-client-template-custom";
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export async function POST(req:Request, res:Request) {
     const body = req ? await req.json() : null
-   const { name, email, tel,subject,message } = body
+    const { name, email, tel,subject,message } = body
     try {
         const dataClient = await resend.emails.send({
             from: 'Kim REMY <onboarding@resend.dev>',
             to: [email],
             subject: 'Kim REMY - Prise en compte de votre demande',
-            react: EmailClientTemplate({ name: name }),
+            react: EmailToClientTemplateCustom({ name: name }),
             text: 'Hello world',
         });
         const dataInfo = await resend.emails.send({
             from: 'Kim REMY <onboarding@resend.dev>',
-            to: ["guillaume@staub.pro"],
+            to: [process.env.NEXT_PUBLIC_EMAIL || 'contact@kimremy.com'],
             subject: `Nouvelle demande depuis le site de ${name}`,
             react: EmailInfoTemplate({ name: name, email: email, tel: tel, subject: subject, message: message }),
             text: 'Hello world',
